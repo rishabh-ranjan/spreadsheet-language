6c314405eefbcc6a554a2acd25be9f030a83bf19
rishabh-ranjan <mr.rishabh.ranjan@gmail.com>
2020-03-10 10:11:39 +0530

    some work

--- sheet.ml
+++ sheet.ml
@@ -45,61 +45,43 @@ let rec col_func func iden ((r1,c1),(r2,c2)) (r,c) sheet =
     sheet.(r).(c) <- Some (r_fold_left iden acc_row r1 r2 sheet);
     col_func func iden ((r1,c1+1),(r2,c2)) (r,c+1) sheet
 
+let count_func x = function
+| None -> x
+| Some _ -> x +. 1.
+
+let full_count = full_func count_func 0.
+let row_count = row_func count_func 0.
+let col_count = col_func count_func 0.
+
+let full_sum = full_func ( +. ) 0.
+let row_sum = row_func ( +. ) 0.
+let col_sum = col_func ( +. ) 0.
+
+let full_avg (((r1,c1),(r2,c2)) as range) ((r,c) as index) sheet =
+    full_sum range index sheet;
+    div_float (index,index) (float ((r2-r1+1)*(c2-c1+1))) index sheet
+
+let row_avg (((r1,c1),(r2,c2)) as range) ((r,c) as index) sheet =
+    row_sum range index sheet;
+    div_float ((r,c),(r+r2-r1,c)) (float (c2-c1+1)) index sheet
+
+let col_avg (((r1,c1),(r2,c2)) as range) ((r,c) as index) sheet =
+    col_sum range index sheet;
+    div_float ((r,c),(r,c+c2-c1)) (float (r2-r1+1)) index sheet
+
+let min_func x y = if y < x then y else x
+let min_iden = max_int (* infinity *)
+let full_min = full_func min_func min_iden
+let row_min = row_func min_func min_iden
+let col_min = col_func min_func min_iden
+
+let max_func x y = if y > x then y else x
+let max_iden = -max_int (* -infinity *)
+let full_max = full_func max_func max_iden
+let row_max = row_func max_func max_iden
+let col_max = col_func max_func max_iden
+
 (**************************************************************)
-
-let full_func func iden range index sheet =
-    let ((r1, c1), (r2, c2)) = range in
-    (* accumulation function for columns *)
-    let acc_col x y = func x y in
-    (* accumulation function for rows *)
-    let acc_row x y =
-        let z = r_fold_left iden acc_col c1 c2 y in
-        func x z
-    in
-    let (r, c) = index in
-    sheet.(r).(c) <- Some (r_fold_left 0. acc_row r1 r2 sheet);
-    sheet
-
-let rec row_func func range index sheet =
-    let ((r1, c1), (r2, c2)) = range in
-    if r1 > r2 then sheet else
-    (* accumulation function for columns *)
-    let acc_col x y = x +. (func y) in
-    let (r, c) = index in
-    sheet.(r).(c) <- Some (r_fold_left 0. acc_col c1 c2 sheet.(r1));
-    row_func func ((r1+1, c1), (r2, c2)) (r+1, c) sheet
-
-let rec col_func func range index sheet =
-    let ((r1, c1), (r2, c2)) = range in
-    if c1 > c2 then sheet else
-    (* accumulation function for rows *)
-    let acc_row x y = x +. y.(c1) in
-    let (r, c) = index in
-    sheet.(r).(c) <- Some (r_fold_left
-
-let acc_row f x y = x +. (r_fold_left 0. (acc_col f) c1
-
-let full_count range index sheet =
-    let ((r1, c1), (r2, c2)) = range in
-    (* accumulating function for columns *)
-    let cf (x: int) (y: float option) =
-        let z = match y with
-        | None -> 0
-        | Some _ -> 1
-        in x + z
-    in
-    (* accumulating function for rows *)
-    let rf (x: int) (y: float option array) =
-        let z = r_fold_left 0 cf c1 c2 y in
-        x + z
-    in
-    let (r, c) = index in
-    sheet.(r).(c) <- Some (float (r_fold_left 0 rf r1 r2 sheet));
-    sheet
-
-let row_count range index sheet =
-    let ((r1, c1), (r2, c2)) = range in
-
 
 let dummy_func_unary r i s = s
 let dummy_func_float r f i s = s
